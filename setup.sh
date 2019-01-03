e8039ae9880d947ac72986042d82f74461024d05
Surya Sankar <suryashankar.m@gmail.com>
2019-01-03 12:59:23 +0530

    added extra packages. Trying to install without conda

--- setup.sh
+++ setup.sh
@@ -3,24 +3,44 @@ set -x #echo on
 sudo apt-get update
 echo "Enter the user name for the administrator. This should be the same as the github user you are going to use"
 read adminuser
-adduser $adminuser
-usermod -aG sudo $adminuser
+sudo adduser $adminuser
+sudo usermod -aG sudo $adminuser
 
-if [ ! -f /opt/anaconda3/bin/conda ]; then
-wget https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh
-bash Anaconda3-2018.12-Linux-x86_64.sh
-rm Anaconda3-2018.12-Linux-x86_64.sh
-fi
+# if [ ! -f /opt/anaconda3/bin/conda ]; then
+# 	wget https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh
+# 	sudo bash Anaconda3-2018.12-Linux-x86_64.sh
+# 	sudo rm Anaconda3-2018.12-Linux-x86_64.sh
+# fi
 
 sudo apt-get -y install python3-pip
 sudo apt-get -y install npm nodejs
 sudo npm install -g configurable-http-proxy
-sudo pip3 install --upgrade setuptools
-sudo pip3 install --upgrade jupyterhub
-sudo pip3 install --upgrade notebook
-sudo pip3 install --upgrade oauthenticator
-sudo pip3 install --upgrade Nikola[extras]
-sudo pip3 install -r requirements.txt
+sudo pip3 install --upgrade setuptools six
+sudo pip3 install --upgrade jupyter jupyterhub jupyterlab oauthenticator Nikola[extras]
+sudo pip3 install --upgrade numpy scipy matplotlib pandas sympy nose
+sudo pip3 install --upgrade Jinja2 packaging pillow python-dateutil PyYAML
+sudo pip3 install --upgrade sqlalchemy tornado bokeh
+
+# sudo pip3 install --upgrade jupyter
+# sudo pip3 install --upgrade jupyterhub
+# # sudo pip3 install --upgrade notebook
+# sudo pip3 install --upgrade jupyterlab
+# sudo pip3 install --upgrade oauthenticator
+# sudo pip3 install --upgrade Nikola[extras]
+Jinja2 >=2.7
+numpy >=1.7.1
+packaging >=16.8
+pillow >=4.0
+python-dateutil >=2.1
+PyYAML >=3.10
+six >=1.5.2
+tornado >=4.3
+
+jupyter labextension install @jupyterlab/git
+pip3 install jupyterlab-git
+jupyter serverextension enable --py jupyterlab_git
+
+jupyter labextension install @jupyterlab/hub-extension
 
 cd /home/$adminuser
 echo Enter the name that you want to give your data dashboard folder
@@ -34,11 +54,11 @@ read blogdomain
 echo Enter the fully qualified domain name to use for the jupyterhub dashboard
 read jupyterhubdomain
 
-openssl req -x509 -nodes -newkey rsa:4096 -keyout $blogdomain.key -out $blogdomain_cert.pem -days 365
+openssl req -x509 -nodes -newkey rsa:4096 -keyout ${blogdomain}.key -out ${blogdomain}_cert.pem -days 365
 mkdir -p /etc/ssl/private/$blogdomain
 mv $blogdomain_cert.pem $blogdomain.key /etc/ssl/private/$blogdomain/
 
-openssl req -x509 -nodes -newkey rsa:4096 -keyout $jupyterhubdomain.key -out $jupyterhubdomain_cert.pem -days 365
+openssl req -x509 -nodes -newkey rsa:4096 -keyout ${jupyterhubdomain}.key -out ${jupyterhubdomain}_cert.pem -days 365
 mkdir -p /etc/ssl/private/$jupyterhubdomain
 mv $jupyterhubdomain_cert.pem $jupyterhubdomain.key /etc/ssl/private/$jupyterhubdomain/
 
@@ -47,7 +67,7 @@ server {
     listen 80;
     listen 443 ssl;
     ssl on;
-    ssl_certificate /etc/ssl/private/$blogdomain/$blogdomain_cert.pem;
+    ssl_certificate /etc/ssl/private/$blogdomain/${blogdomain}_cert.pem;
     ssl_certificate_key /etc/ssl/private/$blogdomain/$blogdomain.key;
     server_name $blogdomain;
     root /home/$adminuser/$reponame/output;
@@ -61,17 +81,17 @@ server {
     listen 80;
     listen 443 ssl;
     ssl on;
-    ssl_certificate /etc/ssl/private/$jupyterhubdomain/$jupyterhubdomain_cert.pem;
+    ssl_certificate /etc/ssl/private/$jupyterhubdomain/${jupyterhubdomain}_cert.pem;
     ssl_certificate_key /etc/ssl/private/$jupyterhubdomain/$jupyterhubdomain.key;
     server_name $jupyterhubdomain;
 
     location / {
-        proxy_pass http://localhost:8000;
+        proxy_pass https://127.0.0.1:8000;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;        
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;        
     }
 }
 EOM
@@ -85,7 +105,7 @@ Description=Jupyterhub
 
 [Service]
 User=root
-Environment="PATH=/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:$conda_bin"
+Environment="PATH=/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin"
 ExecStart=/usr/local/bin/jupyterhub -f /home/$adminuser/$reponame/jupyterhub_config.py
 
 [Install]
@@ -107,14 +127,16 @@ cat <<EOM >> jupyterhub_config.py
 c.JupyterHub.admin_users = {'$adminuser'}
 c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
 c.GitHubOAuthenticator.oauth_callback_url = 'https://$jupyterhubdomain/hub/oauth_callback'
-c.GitHubOAuthenticator.client_id = '$github_client_id'
-c.GitHubOAuthenticator.client_secret = '$github_client_secret'
+c.GitHubOAuthenticator.client_id = '${github_client_id}'
+c.GitHubOAuthenticator.client_secret = '${github_client_secret}'
 c.JupyterHub.proxy_cmd = ['/usr/local/bin/configurable-http-proxy']
-c.JupyterHub.ssl_cert = '/etc/ssl/private/$jupyterhubdomain/$jupyterhubdomain_cert.pem'
+c.JupyterHub.ssl_cert = '/etc/ssl/private/$jupyterhubdomain/${jupyterhubdomain}_cert.pem'
 c.JupyterHub.ssl_key = '/etc/ssl/private/$jupyterhubdomain/$jupyterhubdomain.key'
 c.Spawner.notebook_dir = '~/$reponame'
 c.Authenticator.whitelist = {'$adminuser'}
 c.LocalAuthenticator.create_system_users = True
+c.JupyterHub.ip = '127.0.0.1'
+c.Spawner.default_url = '/lab'
 EOM
 cat <<EOM > .gitignore
 *.pid
